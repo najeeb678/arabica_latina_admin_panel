b40441835edc84bf78d4c76c62f8a1e623b25303
AliHassanKharal <raialihassan7867@gmail.com>
2025-01-22 16:41:05 +0500

    test

--- src/redux/api/CategoriesAPI.ts
+++ src/redux/api/CategoriesAPI.ts
@@ -2,7 +2,7 @@ import api from "@/services/api";
 
 
 export const getCategories = async () => {
-  const response = await api.get("/products");
-  console.log("rrr in api", response.data)
+  const response = await api.get("/categories");
+  //console.log("rrr in api", response.data)
   return response.data;
 };

4b9b801fa22ae7e0e9b29c642e871f4c178f77ee
AliHassanKharal <raialihassan7867@gmail.com>
2025-01-24 17:35:04 +0500

    update

--- src/types/types.ts
+++ src/types/types.ts
@@ -24,21 +24,6 @@ export type FilterConfig = {
   onChange: (value: string) => void;
   sx?: { [key: string]: any }; // Optional sx prop for custom styles
 };
-export type ButtonConfig = {
-  label: string;
-  variant?: "contained" | "outlined" | "text";
-  size?: "sm" | "md" | "lg" | "full";
-  isDisabled?: boolean;
-  leadingIcon?: React.ReactNode;
-  trailingIcon?: React.ReactNode;
-  type?: "button" | "submit";
-  loading?: boolean;
-  altStyle?: boolean;
-  outlinedAlt?: boolean;
-  textColored?: boolean;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; // Updated to accept an event
-  sx?: SxProps<Theme>;
-};
 
 export type Patient = {
   Sr_No: number;
@@ -73,7 +58,6 @@ export type Services = {
   IMAGE: string;
   STATUS: string;
 };
-import { SxProps, Theme } from "@mui/material";
 
 export type Category = {
    Sr_No: number; 
@@ -106,12 +90,10 @@ export type Category = {
   variantId: string;
   color: string;
   size: string;
-  stock: number;
+  stock: number;  
   price: number;
   isDuotone: boolean;
   createdAt: string;
   updatedAt: string;
-  product: {
-    name: string;
-  }; // Ensure this matches your structure
+  product: any
 };

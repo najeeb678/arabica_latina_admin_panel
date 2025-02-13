7bdc245c1715ed37ee344b7b56b9073d52b5c5cd
hassantahir7 <hassantahir3556@gmail.com>
2025-02-13 19:16:03 +0500

    inquiry api fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://api.arabiclatina.com/",
   headers: {
     "Content-Type": "application/json",
   },
